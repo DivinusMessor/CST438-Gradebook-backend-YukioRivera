608aa43b429b63f29ebf5a694264294c38584dd9
Yukio Rivera <DivinusMessor@users.noreply.github.com>
2023-03-25 20:56:47 -0700

    Update RegistrationServiceMQ.java

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -38,22 +38,30 @@ public class RegistrationServiceMQ extends RegistrationService {
 	//receiver of messages from Registration service
 	@RabbitListener(queues = "gradebook-queue")
 	@Transactional
+	// Receive an EnrollmentDTO object and process it to create a new enrollment
 	public void receive(EnrollmentDTO enrollmentDTO) {
+	    // Find the course in the courseRepository using the course_id provided in the enrollmentDTO
 	    Course course = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+
+	    // Create a new Enrollment object
 	    Enrollment newEnrollment = new Enrollment();
-	    
+
+	    // Set the student email from the received enrollmentDTO
 	    newEnrollment.setStudentEmail(enrollmentDTO.studentEmail);
+	    // Set the student name from the received enrollmentDTO
 	    newEnrollment.setStudentName(enrollmentDTO.studentName);
+	    // Set the found course to the Enrollment object
 	    newEnrollment.setCourse(course);
-	    
-	    enrollmentRepository.save(newEnrollment);
-	    
+
+	    // Save the new enrollment in the enrollmentRepository
+	    enrollmentRepository.save(newEnrollment);    
 	}
 
 	// sender of messages to Registration Service
 	@Override
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
-		// TODO  complete this method in homework 4
+		
+		// Send the courseDTO to the registrationQueue using RabbitMQ
 		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
 	}
 }
