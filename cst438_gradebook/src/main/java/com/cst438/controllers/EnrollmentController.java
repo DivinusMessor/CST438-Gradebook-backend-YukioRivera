ef9466b8d0cd127c0852f2d5f427757ec6bdf1db
Yukio Rivera <DivinusMessor@users.noreply.github.com>
2023-03-25 20:52:48 -0700

    Update EnrollmentController.java

--- cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -27,22 +27,33 @@ public class EnrollmentController {
 	 * endpoint used by registration service to add an enrollment to an existing
 	 * course.
 	 */
-	@PostMapping("/enrollment")
-	@Transactional
+	@PostMapping("/enrollment") // Define a new POST endpoint to handle enrollment requests
+	@Transactional // Use @Transactional annotation to ensure the enrollment process is atomic and can be rolled back in case of errors
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
 		
-		Enrollment e = new Enrollment();
-		e.setStudentEmail(enrollmentDTO.studentEmail);
-		e.setStudentName(enrollmentDTO.studentName);
-		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
-		if (c==null) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found");
-		}
-		e.setCourse(c);
-		e = enrollmentRepository.save(e);
-		enrollmentDTO.id = e.getId();
-		
-		return enrollmentDTO;
+	    // Create a new Enrollment object
+	    Enrollment e = new Enrollment();
+	    // Set the student email from the received enrollmentDTO
+	    e.setStudentEmail(enrollmentDTO.studentEmail);
+	    // Set the student name from the received enrollmentDTO
+	    e.setStudentName(enrollmentDTO.studentName);
+
+	    // Find the course in the courseRepository by the course_id provided in the enrollmentDTO
+	    Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+	    // If the course is not found, throw an exception with a BAD_REQUEST status
+	    if (c==null) {
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found");
+	    }
+	    // Set the found course to the Enrollment object
+	    e.setCourse(c);
+
+	    // Save the enrollment in the enrollmentRepository and return the saved instance
+	    e = enrollmentRepository.save(e);
+	    // Set the id of the enrollmentDTO to the id of the saved Enrollment object
+	    enrollmentDTO.id = e.getId();
+
+	    // Return the updated enrollmentDTO to the caller
+	    return enrollmentDTO;
 		
 	}
 
